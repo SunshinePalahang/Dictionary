5ce5c46e335d14a71723f193190979db69909de8
Sunshine Richmer Palahang <sunshinerichmerdpalahang@iskolarngbayan.pup.edu.ph>
2022-11-12 11:20:45 +0800

    Implemented display

---
+++ 1.py
@@ -0,0 +1,12 @@
+#display menu
+def menu():
+    print("Menu: ")
+    print("  1 -> Add an item")
+    print("  2 -> Search")
+    print("  3 -> Exit")
+
+menu()
+#ask user input
+#perform option 1, add data
+#perform option 2, search
+#exit
